2cf42b983fdf3caaea4151869994c802637ac273
ZeoXer <andrew17413@gmail.com>
2023-07-14 17:21:04 +0800

    fix responsive workModal display

--- src/components/WorkModal.jsx
+++ src/components/WorkModal.jsx
@@ -16,14 +16,14 @@ const WorkModal = ({ onClose, work }) => {
         onClick={onClose}
         className="fixed inset-0 bg-gray-200 opacity-80 z-40"
       ></div>
-      <div className="fixed inset-y-40 md:inset-y-60 lg:inset-y-20 inset-x-10 md:inset-x-20 lg:inset-x-80 p-10 rounded-xl bg-white border-slate-400 border-2 z-50">
-        <div className="md:flex justify-between items-center h-full">
-          <img className="w-6/12 rounded-xl" src={work.img} alt="work-img" />
-          <div className="w-5/12">
+      <div className="fixed inset-y-20 md:inset-y-60 lg:inset-y-20 inset-x-10 md:inset-x-20 lg:inset-x-80 p-10 rounded-xl bg-white border-slate-400 border-2 z-50">
+        <div className="mt-5 md:mt-0 md:flex justify-between items-center h-full">
+          <img className="mb-5 md:mb-0 md:w-6/12 rounded-xl" src={work.img} alt="work-img" />
+          <div className="md:w-5/12">
             <h4 className="text-xl font-bold mb-1">{work.title}</h4>
             <p className="text-lg">{work.description}</p>
           </div>
-          <div className="absolute top-5 right-5 border-b-2 border-slate-400">
+          <div className="absolute top-3 right-3 md:top-5 md:right-5 border-b-2 border-slate-400">
             {work.demo !== null && (
               <a
                 href={work.demo}
