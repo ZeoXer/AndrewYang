ca498e0d2b035d86a8f619bd9aa374a91b274497
ZeoXer <andrew17413@gmail.com>
2023-07-14 17:54:50 +0800

    fix header bg

--- src/containers/PersonalInfo.jsx
+++ src/containers/PersonalInfo.jsx
@@ -7,15 +7,9 @@ const PersonalInfo = ({ id, bgName }) => {
       <div className="flex items-center h-full">
         <div>
           <Title textColor="text-cyan-950">楊佳勳</Title>
-          <p className="text-xl md:text-3xl text-amber-300 md:text-slate-400">
-            不畏挑戰並能快速適應
-          </p>
-          <p className="text-xl md:text-3xl text-amber-300 md:text-slate-400">
-            對資訊領域充滿熱忱和毅力
-          </p>
-          <p className="text-xl md:text-3xl text-amber-300 md:text-slate-400">
-            增長多向技能樹，開拓全新賽道
-          </p>
+          <p className="text-xl md:text-3xl">不畏挑戰並能快速適應</p>
+          <p className="text-xl md:text-3xl">對資訊領域充滿熱忱和毅力</p>
+          <p className="text-xl md:text-3xl">增長多向技能樹，開拓全新賽道</p>
         </div>
       </div>
     </ContentBlock>
