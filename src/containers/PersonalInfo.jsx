b6c4164e477246a7180d18a42e793da76b6f15b1
ZeoXer <andrew17413@gmail.com>
2023-07-14 15:30:18 +0800

    fix responsive bg

--- src/containers/PersonalInfo.jsx
+++ src/containers/PersonalInfo.jsx
@@ -7,9 +7,13 @@ const PersonalInfo = ({ id, bgName }) => {
       <div className="flex items-center h-full">
         <div>
           <Title textColor="text-cyan-950">楊佳勳</Title>
-          <p className="text-xl md:text-3xl text-slate-400">不畏挑戰並能快速適應</p>
-          <p className="text-xl md:text-3xl text-slate-400">對資訊領域充滿熱忱和毅力</p>
-          <p className="text-xl md:text-3xl text-slate-400">
+          <p className="text-xl md:text-3xl text-amber-300 md:text-slate-400">
+            不畏挑戰並能快速適應
+          </p>
+          <p className="text-xl md:text-3xl text-amber-300 md:text-slate-400">
+            對資訊領域充滿熱忱和毅力
+          </p>
+          <p className="text-xl md:text-3xl text-amber-300 md:text-slate-400">
             增長多向技能樹，開拓全新賽道
           </p>
         </div>
